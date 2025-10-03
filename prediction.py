e401fc0c0c02a4a86ecd9a08eba8b37931108b3e
socallmebertille <sarah.bejard@gmail.com>
2025-10-03 23:23:55 +0200

    second assigment starts

--- prediction.py
+++ prediction.py
@@ -1,24 +1,26 @@
-import sys
-import json
+import sys, json
 
-def estimate_price(mileage):
-    try:
-        with open('./theta.json', 'r') as file:
-            data = json.load(file)
-        theta = [data["theta0"], data["theta1"]]
-        return theta[0] + (theta[1] * mileage)
-    except Exception as e:
-        print(f"Error when loading theta.json: {e}.")
-        sys.exit(1)
+def estimate_price(mileage, theta0, theta1):
+    return theta0 + (theta1 * mileage)
 
 def main():
     if len(sys.argv) != 2:
         print("Usage: python prediction.py <given mileage>")
         return 1
 
+    # get actual thetas
+    try:
+        with open('./theta.json', 'r') as file:
+            data = json.load(file)
+        theta = [data["theta0"], data["theta1"]]
+    except Exception as e:
+        print(f"Error when loading theta.json: {e}")
+        sys.exit(1)
+
+    # get estimate price
     try:
         float(sys.argv[1])
-        price = estimate_price(float(sys.argv[1]))
+        price = estimate_price(float(sys.argv[1]), theta[0], theta[1])
         print(f"For {sys.argv[1]} miles, the price is estimated at {price:.2f}$.")
     except ValueError:
         print("The argument given is not numeric:", sys.argv[1])
--- training.py
+++ training.py
@@ -1,7 +1,58 @@
-import sys
+import sys, json, csv, prediction
+
+def get_theta():
+    try:
+        with open('./theta.json', 'r') as file:
+            data = json.load(file)
+        theta = [data["theta0"], data["theta1"]]
+        return theta[0], theta[1]
+    except Exception as e:
+        print(f"Error when loading theta.json: {e}")
+        sys.exit(1)
+
+def gradient_descent_algorithm(data, theta0, theta1, learningRate=0.07):
+    sum = 0
+    for elem in data:
+        sum += prediction.estimate_price(elem[0], theta0, theta1) - elem[1]
+    tmp0 = learningRate * (sum/len(data)) if len(data) > 0 else None
+    theta0 -= tmp0
+    sum = 0
+    for elem in data:
+        sum += (prediction.estimate_price(elem[0], theta0, theta1) - elem[1]) * elem[0]
+    tmp1 = learningRate * (sum/len(data)) if len(data) > 0 else None
+    theta1 -= tmp1
+    return theta0, theta1
 
 def main():
-    
+    # get data
+    try:
+        data = []
+        with open('./data.csv', 'r') as file:
+            reader = csv.reader(file)
+            next(reader) # skip header
+            for line in reader:
+                mileage = float(line[0])
+                price = float(line[1])
+                data.append((mileage, price))
+    except Exception as e:
+        print(f"Error when loading data.csv: {e}.")
+        sys.exit(1)
+
+    # get actual thetas
+    theta0, theta1 = get_theta()
+    print(f"Old thetas: {theta0:.2f} {theta1:.2f}")
+
+    # update thetas's values
+    theta0, theta1 = gradient_descent_algorithm(data, theta0, theta1)
+    print(f"New thetas: {theta0:.2f} {theta1:.2f}")
+
+    # update theta.json
+    try:
+        with open('./theta.json', 'w') as file:
+            json.dump({"theta0": theta0, "theta1": theta1}, file)
+    except Exception as e:
+        print(f"Error when saving theta.json: {e}.")
+        sys.exit(1)
 
     return 0
 
