cd9ca996484154f0d92635b6363f33c14f8b0b5b
socallmebertille <sarah.bejard@gmail.com>
2025-10-03 22:01:39 +0200

    first assigment done

---
+++ prediction.py
@@ -0,0 +1,30 @@
+import sys
+import json
+
+def estimate_price(mileage):
+    try:
+        with open('./theta.json', 'r') as file:
+            data = json.load(file)
+        theta = [data["theta0"], data["theta1"]]
+        return theta[0] + (theta[1] * mileage)
+    except Exception as e:
+        print(f"Error when loading theta.json: {e}.")
+        sys.exit(1)
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python prediction.py <given mileage>")
+        return 1
+
+    try:
+        float(sys.argv[1])
+        price = estimate_price(float(sys.argv[1]))
+        print(f"For {sys.argv[1]} miles, the price is estimated at {price:.2f}$.")
+    except ValueError:
+        print("The argument given is not numeric:", sys.argv[1])
+        return 1
+
+    return 0
+
+if __name__ == "__main__":
+    main()
